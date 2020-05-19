7221d7378ab2c162d1117a808710218e9116bb01
Alexander Sierra <alexandercsierra@gmail.com>
2020-05-19 12:32:42 -0400

    working count sort

--- src/iterative_sorting/iterative_sorting.py
+++ src/iterative_sorting/iterative_sorting.py
@@ -34,31 +34,64 @@ def bubble_sort(arr):
 
 
 # STRETCH: implement the Count Sort function below
-def count_sort(arr, maximum=-1):
-    # Your code here
-    count = [0 for x in range(len(arr))]
-    #add correct counts
-    for i in range (len(arr)):
-        count[arr[i]] +=1
-    print('first count', count)
-    #sum them
-    for i in range(1, len(count)):
-        the_sum = count[i-1] + count[i]
-        count[i] = the_sum
-
-    print('second count', count)
-    print('before last', arr)
-    #put them in correct position
-    temp_arr = [0 for x in range(len(arr))]
-    for i in range(1, len(arr)+1):
-        el = arr[len(arr)-i]
-        index = count[el] - 1
-        temp_arr[index] = el
-        count[el] -= 1
-        
-    for i in range(len(arr)):
-        arr[i] = temp_arr[i]
-    return arr
+# def count_sort(arr, maximum=-1):
+#     # Your code here
+#     count = [0 for x in range(len(arr))]
+#     #add correct counts
+#     hasNegatives = False
+#     if len(arr) > 0:
+#         for i in range (len(arr)):
+#             if arr[i] > 0:
+#                 count[arr[i]] +=1
+#             else:
+#                 hasNegatives = True
+#         if hasNegatives == False:
+#             #sum them
+#             for i in range(1, len(count)):
+#                 the_sum = count[i-1] + count[i]
+#                 count[i] = the_sum
+#             #put them in correct position
+#             temp_arr = [0 for x in range(len(arr)+1)]
+#             for i in range(1, len(arr)+1):
+#                 el = arr[len(arr)-i]
+#                 index = count[el] - 1
+#                 temp_arr[index] = el
+#                 count[el] -= 1
+                
+#             for i in range(len(arr)):
+#                 arr[i] = temp_arr[i]
+#             return arr
+#     elif len(arr) == 0:
+#         return arr
+#     else:
+#         return "Error, negative numbers not allowed in Count Sort"
 
 
 # print(count_sort([3,2,1,3], 4))
+
+def count_sort(arr, maximum=-1):
+    if len(arr) == 0:
+        return arr
+    
+    #if maximum is not given, we'll take the max value from the input array
+    if maximum == -1:
+        maximum = max(arr)
+
+    #make a bunch of buckets (add one to include max value)
+    #underscore is common when not actually using the iterator
+    buckets = [0 for _ in range(maximum+1)]
+
+    for x in arr:
+        if x < 0:
+            return "Error, negative numbers not allowed in Count Sort"
+        buckets[x] += 1
+
+    #loop through our buckets, starting at the smallest index
+    j=0
+    for i in range(len(buckets)):
+        while buckets[i] > 0:
+            arr[j] = i
+            j+=1
+            buckets[i] -=1
+
+    return arr
