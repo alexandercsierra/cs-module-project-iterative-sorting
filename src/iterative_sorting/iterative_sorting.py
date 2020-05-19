c2863afc7bdcaff6d1f87ee87417dedaecf139ad
Alexander Sierra <AlexanderCSierra@alexanderrasmbp.lan>
2020-05-19 02:55:07 -0400

    some count tests passing

--- src/iterative_sorting/iterative_sorting.py
+++ src/iterative_sorting/iterative_sorting.py
@@ -20,14 +20,15 @@ def selection_sort(arr):
 # TO-DO:  implement the Bubble Sort function below
 def bubble_sort(arr):
     # Your code here
-    for i in range (0, len(arr)-1):
-        for j in range (0,len(arr)-i-1):
-            if arr[j] > arr[j+1]:
-                old_i = arr[i]
-                old_j = arr[j]
-                arr[i] = old_j
-                arr[j] = old_i
-
+    sorted = False
+    while sorted == False:
+        sorted = True
+        for i in range(len(arr)-1):
+            if arr[i] > arr[i+1]:
+                temp = arr[i]
+                arr[i] = arr[i+1]
+                arr[i+1] = temp
+                sorted = False
 
     return arr
 
@@ -35,8 +36,29 @@ def bubble_sort(arr):
 # STRETCH: implement the Count Sort function below
 def count_sort(arr, maximum=-1):
     # Your code here
+    count = [0 for x in range(len(arr))]
+    #add correct counts
+    for i in range (len(arr)):
+        count[arr[i]] +=1
+    print('first count', count)
+    #sum them
+    for i in range(1, len(count)):
+        the_sum = count[i-1] + count[i]
+        count[i] = the_sum
 
-
+    print('second count', count)
+    print('before last', arr)
+    #put them in correct position
+    temp_arr = [0 for x in range(len(arr))]
+    for i in range(1, len(arr)+1):
+        el = arr[len(arr)-i]
+        index = count[el] - 1
+        temp_arr[index] = el
+        count[el] -= 1
+        
+    for i in range(len(arr)):
+        arr[i] = temp_arr[i]
     return arr
 
 
+# print(count_sort([3,2,1,3], 4))
